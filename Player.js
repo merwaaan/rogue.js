1b412376467357083c107e61b4e5f913205b723c
FMdKdD <fmdkdd@gmail.com>
2010-07-24 21:52:43 +0200

    Added more than 3 ways to move around

--- Player.js
+++ Player.js
@@ -76,19 +76,55 @@ Player.prototype =
         var xMove = 0;
         var yMove = 0;
 
+        // TODO: keyCode are not the same for FF
+        // numpad only works in Chrome for the moment
         switch(event.keyCode)
         {
+        // left arrow
         case 37:
+        // numpad 4
+        case 100:
             xMove = -1;
             break;
+        // up arrow
         case 38:
+        // numpad 8
+        case 104:
             yMove = -1;
             break;
+        // right arrow
         case 39:
+        // numpad 6
+        case 102:
             xMove = 1;
             break;
+        // down arrow
         case 40:
+        // numpad 2
+        case 98:
             yMove = 1;
+            break;
+
+        // diagonals
+        // numpad 1
+        case 97:
+            xMove = -1;
+            yMove =  1;
+            break;
+        // numpad 3
+        case 99:
+            xMove =  1;
+            yMove =  1;
+            break;
+        // numpad 7
+        case 103:
+            xMove = -1;
+            yMove = -1;
+            break;
+        // numpad 9
+        case 105:
+            xMove =  1;
+            yMove = -1;
             break;
         }
 
